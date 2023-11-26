07d291e08dce552221f44a8c37974bf7f5ad6dbf
Isaque Ramos Chaves <isaque.chaves@escolaconquer.com.br>
2023-11-26 10:56:22 -0300

    style: change button to Trainings component

--- src/app/trainings/page.tsx
+++ src/app/trainings/page.tsx
@@ -4,14 +4,10 @@ const Trainings = () => {
     return (
         <div className="flex flex-col h-full items-center justify-center p-4 gap-4">
             <p className="text-xl">Nenhum treino encontrado!</p>
-            <div className="w-[70%] flex flex-row gap-2 py-4 bg-[#0B46C7] border rounded-full justify-center">
+            <button className="w-[70%] flex flex-row gap-2 py-4 bg-[#0B46C7] border rounded-full justify-center">
                 <PlusIcon />
-                <button>
-                    <p className="uppercase text-sm font-bold">Adicionar treino</p>
-                </button>
-                
-            </div>
-
+                <p className="uppercase text-sm font-bold">Adicionar treino</p>
+            </button>
         </div>
 
     );
