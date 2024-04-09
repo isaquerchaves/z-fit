61f947f165986680127c5a32e8fd2b0f9e16d5c6
Isaque Chaves <isaque.chaves@escolaconquer.com.br>
2024-04-09 19:15:09 -0300

    feat: new Muscles page

---
+++ src/app/muscles/components/muscle-card.tsx
@@ -0,0 +1,34 @@
+import { useFetchMuscles } from "@/app/hooks/useFetchMuscles";
+import Image from "next/image";
+import Link from "next/link";
+
+const MuscleCard = () => {
+  const muscles = useFetchMuscles();
+
+  return (
+    <Link href={`/muscle/=`} className="grid grid-cols-2 gap-8 lg:grid-cols-3">
+      {muscles.map((muscle: any) => (
+        <div key={muscle.ID} className="flex flex-col">
+          <div className="flex h-[150px] w-full items-center justify-center bg-[#0B46C7] rounded-t-lg">
+            <Image
+              src={muscle.ImageURL}
+              alt={muscle.Name}
+              width={0}
+              height={0}
+              sizes="100vw"
+              className="h-auto max-h-[70%] w-auto max-w-[80%]"
+              style={{
+                objectFit: "contain",
+              }}
+            />
+          </div>
+          <div className="py-3 bg-accent rounded-b-lg">
+            <p className="text-center text-sm font-semibold">{muscle.Name}</p>
+          </div>
+        </div>
+      ))}
+    </Link>
+  );
+};
+
+export default MuscleCard;
---
+++ src/app/muscles/page.tsx
@@ -0,0 +1,17 @@
+'use client'
+import React from 'react';
+import MuscleCard from './components/muscle-card';
+import { ScrollArea } from '@/components/ui/scroll-area';
+
+function Muscles() {
+
+  return (
+    <ScrollArea >
+      <div className="flex flex-col gap-8 p-5 lg:container">
+          <MuscleCard />
+      </div>
+    </ScrollArea>
+  );
+}
+
+export default Muscles;
